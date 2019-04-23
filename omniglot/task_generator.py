f821e9f11e8bdd637f61d26303db71cb0961f77a
faizan.sh <43760911+faixan23@users.noreply.github.com>
2019-04-23 18:07:01 +0500

    change task gen reverted

--- omniglot/task_generator.py
+++ omniglot/task_generator.py
@@ -142,12 +142,15 @@ class ClassBalancedSampler(Sampler):
 def get_data_loader(task, num_per_class=1, split='train',shuffle=True,rotation=0):
     # NOTE: batch size here is # instances PER CLASS
     
-    normalize = transforms.Compose([Rotate(rotation), transforms.ToTensor(),
-      transforms.Normalize(mean=[0.92206, 0.92206, 0.92206], std=[0.08426, 0.08426, 0.08426])
-    ])
+    normalize = transforms.Normalize(mean=[0.92206, 0.92206, 0.92206], std=[0.08426, 0.08426, 0.08426])
+
+#     normalize = transforms.Compose([Rotate(rotation), transforms.ToTensor(),
+#       transforms.Normalize(mean=[0.92206, 0.92206, 0.92206], std=[0.08426, 0.08426, 0.08426])
+#     ])
 #     normalize = transforms.Normalize(mean=[0.92206, 0.92206, 0.92206], std=[0.08426, 0.08426, 0.08426])
 
-    dataset = Omniglot(task,split=split,transform=normalize)
+    dataset = Omniglot(task,split=split,transform=transforms.Compose([Rotate(rotation),transforms.ToTensor(),normalize]))
+#     dataset = Omniglot(task,split=split,transform=normalize)
 
     if split == 'train':
         sampler = ClassBalancedSampler(num_per_class, task.num_classes, task.train_num,shuffle=shuffle)
